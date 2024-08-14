ce2f9501c2ec0b6ea3d7ad8611ae0f254b085513
Midhun <midhunskani@gmail.com>
2024-08-14 15:45:35 +0530

    EWIRE-14--Models for Like

--- posts/admin.py
+++ posts/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from django.core.checks import Tags
 
-from .models import Post, Tag
+from .models import Post, Tag, Like
+
 
 # Register your models here.
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at', 'published', 'likes_count')
+    filter_horizontal = ('tags',)
+
+
 admin.site.register(Tag)
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'created_at')
