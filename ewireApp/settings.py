9bbb08a4180d5bfc0013b6cae4818331672e7c1c
Midhun <midhunskani@gmail.com>
2024-08-14 09:31:43 +0530

    EWIRE-6--Implement Token Based AUTH

--- ewireApp/settings.py
+++ ewireApp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'users',
     'drf_yasg',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -56,8 +57,7 @@ ROOT_URLCONF = 'ewireApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
