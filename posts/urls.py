611b2cd3a45064335c5e19851b023d27073a52cd
Midhun <midhunskani@gmail.com>
2024-08-14 16:25:46 +0530

    EWIRE-15--Create the API so that the user can like or unlike a post

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import PostViewSet
+from .views import PostViewSet, LikeViewSet
 
 router = DefaultRouter()
 router.register('posts', PostViewSet, basename='post')
+router.register('likes', LikeViewSet, basename='like')
 
 urlpatterns = [
     path('api/', include(router.urls)),
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,8 @@
 from rest_framework import viewsets, permissions
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 
-from .models import Post
-from .serializers import PostSerializer
+from .models import Post, Like
+from .serializers import PostSerializer, LikeSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -10,3 +10,10 @@ class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [TokenAuthentication, SessionAuthentication]
     queryset = Post.objects.all().order_by('id')
+
+
+class LikeViewSet(viewsets.ModelViewSet):
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
+    queryset = Like.objects.all().order_by('id')
