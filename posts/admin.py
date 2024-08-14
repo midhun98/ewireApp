3b3ab3967d5ad8c52e16da382a09282398dbf7c8
Midhun <midhunskani@gmail.com>
2024-08-14 14:14:44 +0530

    EWIRE-9--Create Posts App

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.core.checks import Tags
+
+from .models import Post, Tag
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Tag)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from users.models import CustomUser
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    tags = models.ManyToManyField('Tag', related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
+    published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
